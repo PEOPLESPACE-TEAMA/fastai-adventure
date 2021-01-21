157ed2623cb57ae518bce73ff55f45cf291cbba3
yulaseo <yula0821@naver.com>
2021-01-21 16:27:51 +0900

    Add urls

--- stock/urls.py
+++ stock/urls.py
@@ -5,4 +5,15 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', views.main, name='main'),
+    path('signup/', views.signup, name='signup'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
+    path('home/', views.home, name='home'),
+    path('bookmark/', views.bookmark, name='bookmark'), # 비회원 북마크
+    path('bookmark/<int:user_id>/', views.bookmark, name='bookmark'), # 회원 북마크
+    path('bookmark-list/', views.bookmark_list, name='bookmark_list'),
+    path('bookmark-list/<int:user_id>/', views.bookmark_list, name='bookmark_list'),
+    path('market/', views.market, name='market'),
+    path('market-list/', views.market_list, name='market_list'),
+    path('stock/<int:pk>/', views.stock_detail, name='stock_detail'),
 ]
--- stock/views.py
+++ stock/views.py
@@ -1,6 +1,33 @@
 from django.shortcuts import render
 
-# Create your views here.
 
 def main(request):
     return render(request, 'stock/main.html')
+
+def signup(request):
+    return render(request, 'stock/signup.html')
+
+def login(request):
+    return render(request, 'stock/login.html')
+
+def logout(request):
+    # 로그아웃 하면 로그인 화면으로 연결
+    return render(request, 'stock/login.html')
+
+def home(request):
+    return render(request, 'stock/home.html')
+
+def bookmark(request):
+    return render(request, 'stock/bookmark.html')
+
+def bookmark_list(request):
+    return render(request, 'stock/bookmark_list.html')
+
+def market(request):
+    return render(request, 'stock/market.html')
+
+def market_list(request):
+    return render(request, 'stock/market_list.html')
+
+def stock_detail(request):
+    return render(request, 'stock/stock_detail.html')
