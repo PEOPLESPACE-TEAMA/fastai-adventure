9a83858a2ba98bdb4cb945d0ba4c97e73f3f2ed3
신동준 <shindj4613@outlook.com>
2021-01-22 23:39:14 +0900

    이메일 보내는 메소드 추가

--- config/settings.py
+++ config/settings.py
@@ -119,3 +119,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+#sendEmail
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.googlemail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'autota87@gmail.com'
+EMAIL_HOST_PASSWORD = 'wkehd123'
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
