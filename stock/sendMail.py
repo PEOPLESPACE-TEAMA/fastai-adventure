06fe36264792325ff3e3c6608708140f92aae474
신동준 <shindj4613@outlook.com>
2021-02-03 17:22:28 +0900

    mail

--- stock/sendMail.py
+++ stock/sendMail.py
@@ -1,5 +1,7 @@
 from django.core.mail import EmailMessage
+from email.mime.image import MIMEImage
 from django.template.loader import render_to_string
+from django.core.mail import EmailMultiAlternatives
 
 def sendMail(title, contents, adress):
     '''
@@ -9,6 +11,10 @@ def sendMail(title, contents, adress):
     '''
     #media = 'stock/mail_template_example/regular/images'
     contents = render_to_string('stock/mail_template_example/regular/email.html') #일단 이렇게 하면 메일이 html로 가기는 하는데 이미지는 안들어감 이미지 포함해서 보내는 방법을 알아내야 함 
-    email = EmailMessage(title, contents, to=[adress])
+    email = EmailMultiAlternatives(title, contents, to=[adress])
+    #email = EmailMessage(title, contents, to=[adress])
     email.content_subtype = 'html'
     email.send()    
+
+    #이거 html로 보내기는 가능한데 html파일에 있는 이미지는 같이 안보내짐 어떻게 이미지 넣어서 html로 메일 보내는지 모르겠음
+    #이미지 못 넣으면 html로 그래프를 그리는게 가능하다면 html로 그래프를 그러서 넣을수 있음녀 좋을 듯 
